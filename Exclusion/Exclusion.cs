c6f05bbe08de39404f2e1d0c0c44c0bb50528571
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-01-17 14:29:37 +0400

    Add files via upload

---
+++ Exclusion/Exclusion.cs
@@ -0,0 +1,23 @@
+﻿namespace Exclusions;
+
+public class MException : Exception
+{
+    public int? ErrorCode { get; }
+
+    public MException()
+    {
+    }
+
+    public MException(string? message) : base(message)
+    {
+    }
+
+    public MException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    public MException(string? message, int errorCode) : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+}
---
+++ Exclusion/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exclusions;
+
+try
+{
+    while (true)
+    {
+        Console.WriteLine("1. Server error");
+        Console.WriteLine("2. Division by zero");
+        Console.WriteLine("3. Array boundary");
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 0:
+                int zero = 0;
+                int result = 1 / zero;
+                break;
+
+            case 1:
+                throw new MException("Not found", 404);
+
+            case 2:
+                var array = Array.Empty<string>();
+                var element = array[1];
+                break;
+        }
+    }
+}
+catch (MException e)
+{
+    Console.WriteLine($"{e.Message} {e.ErrorCode}");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("Division by zero");
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine("Going beyond the boundaries of the array");
+}
