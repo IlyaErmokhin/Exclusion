2576351e2de3f9ae417b31a8a158904f25a6d6b3
IlyaErmokhin <63489397+IlyaErmokhin@users.noreply.github.com>
2023-02-21 22:32:22 +0400

    Update Program.cs

--- Exclusion/Program.cs
+++ Exclusion/Program.cs
@@ -1,39 +1,46 @@
-﻿using Exclusions;
+using Exceptions;
 
-try
+namespace MyException
 {
-    while (true)
+
+    class Program
     {
-        Console.WriteLine("1. Server error");
-        Console.WriteLine("2. Division by zero");
-        Console.WriteLine("3. Array boundary");
-        int choice = int.Parse(Console.ReadLine());
-        switch (choice)
+        static void Main(string[] args)
         {
-            case 0:
-                int zero = 0;
-                int result = 1 / zero;
-                break;
+            Console.WriteLine("1. Division by zero");
+            Console.WriteLine("2. Server error");
+            Console.WriteLine("3. Array boundary");
+            int choice = int.Parse(Console.ReadLine());
+            try
+            {
+                switch (choice)
+                {
+                    case 1:
+                        int zero = 0;
+                        int result = 1 / zero;
+                        break;
 
-            case 1:
-                throw new MException("Not found", 404);
+                    case 2:
+                        throw new MException("Not found", 404);
 
-            case 2:
-                var array = Array.Empty<string>();
-                var element = array[1];
-                break;
+                    case 3:
+                        var array = Array.Empty<string>();
+                        var element = array[1];
+                        break;
+                }
+            }
+            catch (MException e)
+            {
+                Console.WriteLine($"{e.Message} {e.ErrorCode}");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Division by zero");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Going beyond the boundaries of the array");
+            }
         }
     }
-}
-catch (MException e)
-{
-    Console.WriteLine($"{e.Message} {e.ErrorCode}");
-}
-catch (DivideByZeroException e)
-{
-    Console.WriteLine("Division by zero");
-}
-catch (IndexOutOfRangeException e)
-{
-    Console.WriteLine("Going beyond the boundaries of the array");
 }
